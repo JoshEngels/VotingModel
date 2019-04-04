d9c1261a2a98aa3e6aea57804a56f26f6d748dce
Josuha Engels <joshuaengels@yahoo.com>
2019-04-03 21:17:15 -0500

    Cleaned up ballot code some

--- 1Ballot/No-Lines.lisp
+++ 1Ballot/No-Lines.lisp
@@ -238,58 +238,57 @@
   
 	(reset)
   
-	(setq i 0)
-	(setq button-map (make-hash-table)) 
-	(setq button-state (make-hash-table)) 
-	(setq button-index (make-hash-table)) 
-	;(setq noise 1)
-	(setq noise 0)
-	(setq noise_macro 0)
-
   
-	(let* ((window (open-exp-window "Ballet" :width 900 :height 550)))
-	
-	
-		(setq starting-x 10)
+	(let* (
+		(window (open-exp-window "Ballet" :width 900 :height 550)) 
+		(starting-x 10) 
+		(i 0)
+		(j 0)
+		(button-map (make-hash-table)) 
+		(button-state (make-hash-table)) 
+		(button-index (make-hash-table)) 
+		(noise 0)
+		(noise_macro 0))
+		
 	
 		(loop
 	
-			(setq j 0)
-			
-			(setq starting-x (+ (* i 300) 10))
+			(setf j 0)
+			(setf starting-x (+ (* i 300) 10))
 		
 			;starting x is i * 160 + 10
 			
 			(loop 
 
-				;starting y is j * 85 + 10		
-				(setq starting-y (+ (* j 85) 10))
 				
-				
-				
-				(setq randomx (+ starting-x (rand noise_macro)))
-				(setq randomy (+ starting-y (rand noise_macro)))
-
-				
-				
-				(setf candidate-party-object-array (make-array '(6)))
-				
-				(setq race-num (+ (* i 6) j))
-
-
-				(setf contest (pop cntst-lst))
-				(setf candidates (cand-lst contest))
-
-				
-				(add-text-to-exp-window :text (office-name contest) :x randomx :y randomy)
 				
 				
 				; Constructs the ballot
-				(let ((candidate (pop candidates))
-						(y-offset 20)
-						(index 0))
+				(let* (
+
+					(starting-y (+ (* j 85) 10))
 				
+					(randomx (+ starting-x (rand noise_macro)))
+					(randomy (+ starting-y (rand noise_macro)))
 				
+					(candidate-party-object-array (make-array '(6)))
+				
+					(race-num (+ (* i 6) j))
+
+
+					(contest (pop cntst-lst))
+					(candidates (cand-lst contest))
+				
+					(candidate (pop candidates))
+					(y-offset 20)
+					(index 0))
+									
+					
+					; Adds the race name
+					(add-text-to-exp-window :text (office-name contest) :x randomx :y randomy)
+
+				
+					; Constructs the rest of the ballot
 					(loop while candidate 
 			
 					do 	(progn 
@@ -320,72 +319,6 @@
 						(setf y-offset (+ y-offset 15))
 						(setf candidate (pop candidates))
 						(setf index (+ index 1)))))
-						
-						
-				
-				;(setf (aref candidate-party-object-array 0) (add-text-to-exp-window :text (concatenate 'string "candidate" (write-to-string (* race-num 3))) :x (+ randomx 30 (rand noise)) :y (+ randomy 20 (rand noise))))
-				;(setf (aref candidate-party-object-array 1) (add-text-to-exp-window :text (concatenate 'string "candidate" (write-to-string (+ (* race-num 3) 1))) :x (+ randomx 30 (rand noise)) :y (+ randomy 35 (rand noise))))
-				;(setf (aref candidate-party-object-array 2) (add-text-to-exp-window :text (concatenate 'string "candidate" (write-to-string (+ (* race-num 3) 2))) :x (+ randomx 30 (rand noise)) :y (+ randomy 50 (rand noise))))
-				
-				;(setf (aref candidate-party-object-array 3) (add-text-to-exp-window :text "party1" :x (+ randomx 118 (rand noise)) :y (+ randomy 20 (rand noise))))
-				;(setf (aref candidate-party-object-array 4) (add-text-to-exp-window :text "party2" :x (+ randomx 118 (rand noise)) :y (+ randomy 35 (rand noise))))
-				;(setf (aref candidate-party-object-array 5) (add-text-to-exp-window :text "party3" :x (+ randomx 118 (rand noise)) :y (+ randomy 50 (rand noise))))
-				
-				; (setf button_temp (add-button-to-exp-window :text "" :x randomx :y (+ randomy 22) :width 20 :height 10 :action 
-				; (lambda (button)
-				; (if (= (gethash button button-state) 0) (progn
-					; (modify-text-for-exp-window (aref (gethash button button-map) 0) :color 'blue)
-					; (modify-text-for-exp-window (aref (gethash button button-map) 3) :color 'blue)
-					; (setf (gethash button button-state) 1)
-					; )
-					; (progn
-					; (modify-text-for-exp-window (aref (gethash button button-map) 0) :color 'black)
-					; (modify-text-for-exp-window (aref (gethash button button-map) 3) :color 'black)
-					; (setf (gethash button button-state) 0)
-					; )
-				; )
-				; )))
-				; (setf (gethash button_temp button-map) candidate-party-object-array)
-				; (setf (gethash button_temp button-state) 0)
-
-				
-				
-				
-				; (setf button_temp (add-button-to-exp-window :text "" :x randomx :y (+ randomy 38) :width 20 :height 10 :action 
-				; (lambda (button)
-				; (if (= (gethash button button-state) 0) (progn
-					; (modify-text-for-exp-window (aref (gethash button button-map) 1) :color 'blue)
-					; (modify-text-for-exp-window (aref (gethash button button-map) 4) :color 'blue)
-					; (setf (gethash button button-state) 1)
-					; )
-					; (progn
-					; (modify-text-for-exp-window (aref (gethash button button-map) 1) :color 'black)
-					; (modify-text-for-exp-window (aref (gethash button button-map) 4) :color 'black)
-					; (setf (gethash button button-state) 0)
-					; )
-				; )
-				; )))
-				; (setf (gethash button_temp button-map) candidate-party-object-array)
-				; (setf (gethash button_temp button-state) 0)
-				
-				
-				; (setf button_temp (add-button-to-exp-window :text "" :x randomx :y (+ randomy 53) :width 20 :height 10 :action 
-				; (lambda (button)
-				; (if (= (gethash button button-state) 0) (progn
-					; (modify-text-for-exp-window (aref (gethash button button-map) 2) :color 'blue)
-					; (modify-text-for-exp-window (aref (gethash button button-map) 5) :color 'blue)
-					; (setf (gethash button button-state) 1)
-					; )
-					; (progn
-					; (modify-text-for-exp-window (aref (gethash button button-map) 2) :color 'black)
-					; (modify-text-for-exp-window (aref (gethash button button-map) 5) :color 'black)
-					; (setf (gethash button button-state) 0)
-					; )
-				; )
-				; )))
-				; (setf (gethash button_temp button-map) candidate-party-object-array)
-				; (setf (gethash button_temp button-state) 0)
-			
 			
 				(setq j (+ j 1))
 			
