dbaba4f7d5cc895f89188fb9766a33b6d39c369f
Josuha Engels <joshuaengels@yahoo.com>
2019-10-03 15:43:32 -0500

    Relative Positions using color now, simplifying

--- 4Encoding/Relative-Positions-Color.lisp
+++ 4Encoding/Relative-Positions-Color.lisp
@@ -1,44 +1,3 @@
-;;;  -*- mode: LISP; Syntax: COMMON-LISP;  Base: 10 -*-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 
-;;; Author      : Joshua Engels
-;;; Copyright   : (c) 2019 Joshua Engels
-;;; Address     : Lovett College 
-;;;             : Rice University
-;;;             : Houston, TX 77005
-;;;             : jae4@rice.edu
-;;; 
-;;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 
-;;; Filename    : Relative-Positions.lisp
-;;; Version     : 1
-;;; 
-;;; Description : Encoding strategy
-;;;				: * Starts off with visual attention at the race header and determines the race header, candidate, and party groups
-;;;				: * using only relative positions. This means that there are no cheats or look ups in memory; the groups are encoded by
-;;;				: * the structure of a race.
-;;;
-;;; Bugs        : * If the ballot has noise, the relative movement from candidate to party will not work in a race with only one candidate.
-;;;				: * This is because the way the navigation works is kind of a cheat. In order to maintain relative movement, the model finds
-;;;				: * the candidate of lowest y and finds text to the right of it with greater y not in its group. This is neccesary because otherwise
-;;;				: * the nearest text might be below in the next race. This is not a problem if the ballot had "no noise", i.e. if the candidates
-;;;				: * and parties had the exact same y.
-;;;
-;;; To do       : 
-;;;
-;;; ----- History -----
-;;; 2019.4.28   Joshua Engels
-;;;				: * Documented the file
-;;;
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; General Docs:
-;;; See contract.pdf for info on what fields the model has access to from the macronavigation
-;;;   
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Encodes the race and makes a request for a button associated with this race
 ;; No attend production because productions that lead into this have one
@@ -46,7 +5,6 @@
 
  =goal>
 	state   	storing-race-group
-	left		=left-bound
 
 =visual>
 	value		=text
@@ -54,16 +12,9 @@
 =visual-location>
 	ISA			visual-location	
 	kind		text
-	group		=group1
-	screen-y	=screen-y
-	screen-x	=center-x
-	width		=width
-
 
 =imaginal>
 	race-group	none
-
-!bind! =new-left (- (- =center-x (/ =width 2)) 5)
 
 
 ==>
@@ -71,91 +22,18 @@
 +visual-location>
 	ISA			visual-location
 	> screen-y	current
-	> screen-x	=left-bound
-	< screen-x	current
+	color		purple
 	:nearest	current
-	kind		oval
 	
 =imaginal>
 	race-group  =group1
  
 =goal>
-	state		find-button-group
-	left 		=new-left
-	
+	state		find-candidate-group	
 	
 !output! ("Example of race is: ~s" =text)
 
 
-
-)
-
-;; Attends the button 
-(P Attend-Button
-
-=goal>
-	state		find-button-group
-
-=visual-location>
-	ISA			visual-location	
-	kind		oval
-
-?visual>
-	state		free
-	
-==>
-
-+visual>
-	ISA			move-attention
-	screen-pos	=visual-location
-	
-=visual-location>
-	
-=goal>
-	state		storing-button-group
-
-)
-
-; Top and bottom mean something different
-
-; Makes a request for a candidate (text closest to the bubble and to the right of it and not equal to the race group)
-; Does not store its group, but could be added later
-(P Encode-Button
-
- =goal>
-	state   	storing-button-group
-	left		=left-bound
-	top			=top
-
-=visual>
-	
-=visual-location>
-	ISA			visual-location	
-	kind		oval
-	group		=group
-
-
-=imaginal>
-	- race-group	none
-	race-group		=race-group
-	;button-group	none
-
-
-==>
-
-+visual-location>
-	ISA			visual-location
-	> screen-x	=left-bound
-	> screen-y	=top
-	:nearest	current
-	- group		=race-group
-	kind		text
-	
-=imaginal>
-	;button-group  =group
- 
-=goal>
-	state		find-candidate-group
 
 )
 
@@ -185,7 +63,7 @@
 
 )
 
-;; Encodes the candidate group and makes a visual request for the candidate with greatest y
+;; Encodes the candidate group and makes a visual request for the parties
 (P Encode-Candidate
 
  =goal>
@@ -203,7 +81,6 @@
 
 =imaginal>
 	- race-group	none
-	;- button-group	none
 	candidate-group	none
 	race-group		=race-group
 
@@ -212,104 +89,34 @@
 +visual-location>
 	ISA			visual-location
 	kind		text
-	group		=group2
-	screen-y	highest
+	:nearest	current
+	color		blue
 
 =imaginal>
 	candidate-group  =group2
  
 =goal>
-	state		find-party-group-part-1
+	state		find-party-group
 	
 !output! ("Example of candidate is: ~s" =text)
 
 	
 )
 
-; Attends the candidate with greatest y so we can find a party with smaller y
-(P Attend-Different-Candidate
-
-=goal>
-	state		find-party-group-part-1
-
-=visual-location>
-	ISA			visual-location	
-	kind		text
-	screen-y	=bottom
-
-?visual>
-	state		free	
-
-	
-==>
-
-+visual>
-	ISA			move-attention
-	screen-pos	=visual-location
-	
-=visual-location>
-	
-=goal>
-	state		find-party-group-part-2
-	bottom		=bottom
-
-	
-)
-
-; Makes the request to finnaly find the party by requesting a party with y less than or equal to this position
-(P Find-Party
-
-=goal>
-	state   	find-party-group-part-2
-	left		=left-bound
-	top			=top
-	
-=visual>
-
-=imaginal>
-	- race-group		none
-	;- button-group	none
-	- candidate-group	none
-	candidate-group		=candidate-group
-	race-group			=race-group
-
-==>
-
-+visual-location>
-	ISA			visual-location
-	kind		text
-	> screen-x	current
-	<= screen-y	current
-	> screen-y	=top
-	- group		=candidate-group
-	- group		=race-group
-	:nearest	current
- 
-=goal>
-	state		find-party-group-part-3
-	
-=imaginal>
-	
-
-)
-
 ; Attends the party
 (P Attend-Party
 
 =goal>
-	state		find-party-group-part-3
+	state		find-party-group
 
 =visual-location>
 	ISA			visual-location	
 	kind		text
-	width		=width
-	screen-x	=middle-x
+	; width		=width
+	; screen-x	=middle-x
 
 ?visual>
 	state		free
-	
-!bind! =new-right (+ (/ =width 2) =middle-x)
-
 	
 ==>
 
@@ -321,7 +128,6 @@
 	
 =goal>
 	state		storing-party-group
-	right		=new-right
 
 )
 
@@ -345,7 +151,6 @@
 =imaginal>
 	- race-group		none
 	- candidate-group	none
-	;- button-group		none
 	party-group			none
 	
 
